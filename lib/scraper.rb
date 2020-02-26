c6b001dbb38a64b77ea2b540eee607f520ae69dd
Chris Ashman <chrisashman96@gmail.com>
2020-02-26 21:16:39 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,6 +13,8 @@ class Scraper
         student_name = student.css('.student-name').text
         students << {name: student_name, location: student_location, profile_url: student_profile_link}
       end
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
